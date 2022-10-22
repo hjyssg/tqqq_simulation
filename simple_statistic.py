564de7be4b78850e69d1684006f270e33f71b4d7
aji47 <the398355565@gmail.com>
2022-10-23 00:06:36 +0800

    file naming

--- simple_statistic.py
+++ simple_statistic.py
@@ -13,38 +13,39 @@ print(df.shape)
 # float(df[0:1]["Open"])
 # df[df['Date'].dt.year == 2001]
 
-output_data = []
-for year in range(1971, 2022):
-    # df[df[Date] == year + "-11-01"]
-    year_df = df[df['Date'].dt.year == year]
+def calculate_month_11_12():
+    output_data = []
+    for year in range(1971, 2022):
+        # df[df[Date] == year + "-11-01"]
+        year_df = df[df['Date'].dt.year == year]
 
-    early_days = 40
-    first_day = year_df.iloc[0]
-    day_early = year_df.iloc[early_days]
-    last_day = year_df.iloc[-1]
-    # print(year_df.head(10))
+        early_days = 40
+        first_day = year_df.iloc[0]
+        day_early = year_df.iloc[early_days]
+        last_day = year_df.iloc[-1]
+        # print(year_df.head(10))
 
-    # 选择11月
-    sub1 =  year_df[year_df['Date'].dt.month == 11]
-    middle_day = sub1.iloc[0]
+        # 选择11月
+        sub1 =  year_df[year_df['Date'].dt.month == 11]
+        middle_day = sub1.iloc[0]
 
-    year_change = calculate_change(first_day, last_day)
-    change_early = calculate_change(first_day, day_early)
-    ytd_change = calculate_change(first_day, middle_day)
-    rest_change = calculate_change(middle_day, last_day)
-
-
-    output_data.append({
-        "year": year,
-        "全年变化百分比":  round(year_change * 100, 2),
-        ("年初变化百分比 - " + str(early_days)): round(change_early * 100, 2),
-        "前10月变化百分比": round(ytd_change * 100, 2),
-        "最后两个月变化百分比": round(rest_change * 100, 2),
-        "年初价格": first_day["Open"],
-        "年中价格": middle_day["Open"],
-        "年尾价格": last_day["Close"]
-    })
+        year_change = calculate_change(first_day, last_day)
+        change_early = calculate_change(first_day, day_early)
+        ytd_change = calculate_change(first_day, middle_day)
+        rest_change = calculate_change(middle_day, last_day)
 
 
-output_df = pd.DataFrame.from_dict(output_data)
-output_df.to_excel("简易统计.xlsx", index=False)
+        output_data.append({
+            "year": year,
+            "全年变化百分比":  round(year_change * 100, 2),
+            ("年初变化百分比 - " + str(early_days)): round(change_early * 100, 2),
+            "前10月变化百分比": round(ytd_change * 100, 2),
+            "最后两个月变化百分比": round(rest_change * 100, 2),
+            "年初价格": first_day["Open"],
+            "年中价格": middle_day["Open"],
+            "年尾价格": last_day["Close"]
+        })
+
+
+    output_df = pd.DataFrame.from_dict(output_data)
+    output_df.to_excel("简易统计.xlsx", index=False)
