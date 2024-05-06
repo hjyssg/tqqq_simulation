75f6070d8a2c0f1de8b98103f7053d1f504a46b3
hjy <the398355565@gmail.com>
2024-05-06 21:59:44 +0800

    升级了梯子，现在可以用yahoo finance了

--- simple_py/yfinance_download.py
+++ simple_py/yfinance_download.py
@@ -1,35 +1,28 @@
 import yfinance as yf
+import os  # 导入用于处理路径的库
 
-# 将数据保存为CSV文件
+current_script_path = os.path.dirname(__file__)
+directory = os.path.join(current_script_path, "../data")
+
+# 将数据保存为CSV文件的函数
 def save_to_csv(data, filename):
-    data.to_csv(filename)
+    full_path = os.path.join(directory, filename)
+    data.to_csv(full_path)
+    print("数据已保存到", full_path)
 
-
-# 通过股票代码获取数据
+# 通过股票代码获取数据的函数
 def download_stock_data(stock_symbol):
     try:
         data = yf.download(stock_symbol)
         filename = stock_symbol + ".csv"
         save_to_csv(data, filename)
-        print("数据已保存到", filename)
-    except Exception:
-        print(Exception)
+    except Exception as e:
+        print("下载数据失败:", stock_symbol)
+        print("错误:", e)
 
-
-
-# 主程序
+# 主程序，用于下载股票列表中的数据
 if __name__ == "__main__":
-    # 输入你感兴趣的股票代码
-    
-    # 下载股票数据
-    # data = download_stock_data("^SPX")
-
-    # data = download_stock_data("^NDX")
-
-
-    ll = ["^RUT", "NVDA", "soxl", "sqqq", "tqqq", "qyld"]
-    for e in ll:
-        download_stock_data(e)
-    
-
-    
+    # stock_list = ["^NDX", "^SPX"]
+    stock_list = ["^RUT", "^N225", "^HSI"]
+    for stock_symbol in stock_list:
+        download_stock_data(stock_symbol)
