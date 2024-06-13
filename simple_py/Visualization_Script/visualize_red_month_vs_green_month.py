f848b031f423229a06b020c8c40a0159afea6e6d
hjy <the398355565@gmail.com>
2024-06-13 15:24:33 +0800

    update

--- simple_py/Visualization_Script/visualize_red_month_vs_green_month.py
+++ simple_py/Visualization_Script/visualize_red_month_vs_green_month.py
@@ -9,11 +9,12 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import _util
 
 # 导入数据
-data = _util.load_csv_as_dataframe("^NDX.csv")  # data is a DataFrame
+fn = "^SPX.csv"
+data = _util.load_csv_as_dataframe(fn)  # data is a DataFrame
 data['Date'] = pd.to_datetime(data['Date'])  # Ensure 'Date' is a datetime object
 data = data.set_index('Date')  # Set 'Date' as the index
 
-data = data[data.index.year > 2010]
+data = data[data.index.year > 1950]
 
 # 计算周和月的涨跌幅度
 weekly_data = data['Close'].resample('W').last().pct_change() * 100
@@ -31,6 +32,52 @@ print("周涨跌幅度统计特征:")
 print(weekly_data.describe())
 print("\n月涨跌幅度统计特征:")
 print(monthly_data.describe())
+
+
+
+
+
+
+#------------------------------------------------------------------------------------
+def render_histogram(data, title, time_frame):
+    # 过滤掉前1%和后1%的极端值
+    lower_bound = data.quantile(0.01)
+    upper_bound = data.quantile(0.99)
+    data = data[(data >= lower_bound) & (data <= upper_bound)]
+
+    # 可视化设置
+    hist_label = f'{fn} {title}{time_frame}涨跌幅度分布'
+    x_label = '涨跌幅度 (%)'
+    y_label = '频率'
+
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 用黑体显示中文
+    plt.rcParams['axes.unicode_minus'] = False  # 正确显示负号
+
+    # 可视化
+    plt.figure(figsize=(10, 6))
+    sns.histplot(data, kde=True)
+    plt.title(hist_label)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    
+    plt.legend()  # 显示图例
+
+    # 添加mplcursors的tooltip功能
+    # cursor = mplcursors.cursor(hover=True)
+    # cursor.connect("add", lambda sel: sel.annotation.set_text(f'涨跌幅度: {sel.target[0]:.2f}%'))
+
+    plt.savefig(hist_label+".jpg")
+
+    # 显示图表
+    plt.show()
+
+# 渲染周和月的直方图
+render_histogram(weekly_data, '', '周')
+render_histogram(monthly_data, '', '月')
+render_histogram(year_data, '', '年')
+
+
 
 
 # 周涨跌幅度统计特征:
@@ -53,56 +100,5 @@ print(monthly_data.describe())
 # 50%        1.845703
 # 75%        4.743749
 # max       15.191780
-
-#-------------------------------------------------------
-# 计算涨跌幅度的分位数
-quantiles = monthly_data.quantile([0.25, 0.5, 0.75, 0.95])
-print("月度涨跌幅度的主要分位数:")
-print(quantiles)
-
-# 特定的涨跌幅度值
-specific_value = 6.28  # 涨跌幅度为6.28%
-quantile_of_value = monthly_data[monthly_data <= specific_value].count() / monthly_data.count()
-print(f"涨跌幅度 {specific_value}% 的月度分位数位置: {quantile_of_value:.2f}")
-
-#------------------------------------------------------------------------------------
-def render_histogram(data, title, time_frame):
-    # 可视化设置
-    hist_label = f'{title}{time_frame}涨跌幅度分布'
-    x_label = '涨跌幅度 (%)'
-    y_label = '频率'
-
-    plt.rcParams['font.sans-serif'] = ['SimHei']  # 用黑体显示中文
-    plt.rcParams['axes.unicode_minus'] = False  # 正确显示负号
-
-    # 可视化
-    plt.figure(figsize=(10, 6))
-    sns.histplot(data, kde=True)
-    plt.title(hist_label)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-
-    # 计算并标记分位数线
-    quantiles = data.quantile([0.25, 0.5, 0.75, 0.95])
-    colors = ['blue', 'green', 'red', 'purple']  # 分位数线颜色
-    labels = ['25% 分位数', '50% 分位数 (中位数)', '75% 分位数', '95% 分位数']
-    for quantile, color, label in zip(quantiles, colors, labels):
-        plt.axvline(x=quantile, color=color, label=label, linestyle='--')
-    
-    plt.legend()  # 显示图例
-
-    # 添加mplcursors的tooltip功能
-    cursor = mplcursors.cursor(hover=True)
-    cursor.connect("add", lambda sel: sel.annotation.set_text(f'涨跌幅度: {sel.target[0]:.2f}%'))
-
-    # 显示图表
-    plt.show()
-
-# 渲染周和月的直方图
-render_histogram(weekly_data, '', '周')
-render_histogram(monthly_data, '', '月')
-render_histogram(year_data, '', '年')
-
-
 
 
